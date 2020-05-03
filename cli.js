f8a7a2386da980c7412feb826de99dce84143dbe
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-05-03 15:38:09 -0700

    :bug:

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ require("fs").copyFileSync(
     require("path").resolve(process.cwd(), ".xo-config.js"),
 );
 require("fs").copyFileSync(
-    require("path").resolve(__dirname, "pretter.js"),
-    require("path").resolve(process.cwd(), ".prettierrc.js"),
+    require("path").resolve(__dirname, "prettier.js"),
+    require("path").resolve(process.cwd(), "prettier.config.js"),
 );
 console.log("Please run 'npm i -D nature-xo-config' to finish installing, if you haven't already.");
