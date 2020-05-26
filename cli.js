6dd31bd198b6265d08d2aeb310d3a0ad3e751061
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-05-26 09:44:58 -0700

    Add postinstall and remove log

--- cli.js
+++ cli.js
@@ -7,4 +7,3 @@ require("fs").copyFileSync(
     require("path").resolve(__dirname, "prettier.js"),
     require("path").resolve(process.cwd(), "prettier.config.js"),
 );
-console.log("Please run 'npm i -D nature-xo-config' to finish installing, if you haven't already.");
