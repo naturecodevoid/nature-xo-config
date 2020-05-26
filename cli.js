0e44d53582fe1d751a97728ed7c7d1490f54b0a4
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-05-26 10:12:25 -0700

    1.0.6: trying to fix postinstall

--- cli.js
+++ cli.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
-require("fs").copyFileSync(
-    require("path").resolve(__dirname, "exports.js"),
-    require("path").resolve(process.cwd(), ".xo-config.js"),
-);
-require("fs").copyFileSync(
-    require("path").resolve(__dirname, "prettier.js"),
-    require("path").resolve(process.cwd(), "prettier.config.js"),
-);
+if (process.argv.includes("postinstallScript")) {
+    require("fs").copyFileSync(
+        require("path").resolve(__dirname, "exports.js"),
+        require("path").resolve(process.cwd(), "../", "../", ".xo-config.js"),
+    );
+    require("fs").copyFileSync(
+        require("path").resolve(__dirname, "prettier.js"),
+        require("path").resolve(process.cwd(), "../", "../", "prettier.config.js"),
+    );
+} else {
+    require("fs").copyFileSync(
+        require("path").resolve(__dirname, "exports.js"),
+        require("path").resolve(process.cwd(), ".xo-config.js"),
+    );
+    require("fs").copyFileSync(
+        require("path").resolve(__dirname, "prettier.js"),
+        require("path").resolve(process.cwd(), "prettier.config.js"),
+    );
+}
